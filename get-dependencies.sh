0faa5e69160879625696702934c67c25d4dcce1d
juju4 <juju4@users.noreply.github.com>
2017-01-06 16:59:35 -0500

    sync repositories - add Jenkinsfile, galaxy naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,4 +7,10 @@ rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.lxd ] && git clone https://github.com/juju4/ansible-lxd $rolesdir/juju4.lxd
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.lxdconfigure ] && ln -s ansible-lxdconfigure $rolesdir/juju4.lxdconfigure
+[ ! -e $rolesdir/juju4.lxdconfigure ] && cp -R $rolesdir/ansible-lxdconfigure $rolesdir/juju4.lxdconfigure
+
+## don't stop build on this script return code
+true
 
